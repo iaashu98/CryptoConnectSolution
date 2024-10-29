7d085b4d98c4444385ea7d1c1397d1f50a805c09
iaashu98 <ashutoshsamraj1@gmail.com>
2024-10-29 15:49:41 +0530

    commit 6: Added few other cyrpto coins support

--- CryptoConnect/Adapters/BinanceHelper.cs
+++ CryptoConnect/Adapters/BinanceHelper.cs
@@ -1,9 +1,9 @@
 namespace CryptoConnect.Adapters
 {
-    public class BinanceHelper 
+    public class BinanceHelper
     {
-        private static readonly Lazy<BinanceHelper> _instance =   new Lazy<BinanceHelper>(() => new BinanceHelper());
-        private BinanceHelper() { }   
+        private static readonly Lazy<BinanceHelper> _instance = new Lazy<BinanceHelper>(() => new BinanceHelper());
+        private BinanceHelper() { }
         public static BinanceHelper Instance => _instance.Value;
         public string GetCryptoNameFromSymbol(string symbol)
         {
@@ -13,6 +13,22 @@ namespace CryptoConnect.Adapters
                 "ETHUSDT" => "Ethereum",
                 "XRPUSDT" => "Ripple",
                 "LTCUSDT" => "Litecoin",
+                "ADAUSDT" => "Cardano",
+                "DOTUSDT" => "Polkadot",
+                "BNBUSDT" => "Binance Coin",
+                "SOLUSDT" => "Solana",
+                "DOGEUSDT" => "Dogecoin",
+                "SHIBUSDT" => "Shiba Inu",
+                "LINKUSDT" => "Chainlink",
+                "XLMUSDT" => "Stellar",
+                "UNIUSDT" => "Uniswap",
+                "AVAXUSDT" => "Avalanche",
+                "VETUSDT" => "VeChain",
+                "TRXUSDT" => "TRON",
+                "ATOMUSDT" => "Cosmos",
+                "XTZUSDT" => "Tezos",
+                "XMRUSDT" => "Monero",
+                "ALGOUSDT" => "Algorand",
                 _ => "Unknown"
             };
         }
@@ -25,6 +41,22 @@ namespace CryptoConnect.Adapters
                 "ethereum" => "ETHUSDT",
                 "ripple" => "XRPUSDT",
                 "litecoin" => "LTCUSDT",
+                "cardano" => "ADAUSDT",
+                "polkadot" => "DOTUSDT",
+                "binancecoin" => "BNBUSDT",
+                "solana" => "SOLUSDT",
+                "dogecoin" => "DOGEUSDT",
+                "shiba" => "SHIBUSDT",
+                "chainlink" => "LINKUSDT",
+                "stellar" => "XLMUSDT",
+                "uniswap" => "UNIUSDT",
+                "avalanche" => "AVAXUSDT",
+                "vechain" => "VETUSDT",
+                "tron" => "TRXUSDT",
+                "cosmos" => "ATOMUSDT",
+                "tezos" => "XTZUSDT",
+                "monero" => "XMRUSDT",
+                "algorand" => "ALGOUSDT",
                 _ => throw new ArgumentException($"Unsupported cryptoId: {cryptoId}")
             };
         }
